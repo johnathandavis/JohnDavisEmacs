fd679ec9646350a756c41706f6559bdb0f11c63c
Davis <davjohna@amazon.com>
2018-06-14 12:24:55 -0500

    Added all files. Load default theme.

---
+++ SessionDesktop.el
@@ -0,0 +1,44 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Mon Apr 30 10:20:46 2018
+;; Desktop file format version 206
+;; Emacs version 24.4.51.2
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 23) (left . 1280) (unsplittable) (modeline . t) (width . 172) (height . 83) (fringe) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "Red") (background-mode . light) (horizontal-scroll-bars . t) (fullscreen) (alpha) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (line-spacing) (background-color . "White") (foreground-color . "Black") (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(172 83 ((0 0 176 82)) (("~/Desktop/data.template.yml" "data.template.yml" 1 1 ((tab "*scratch*" "/Users/davjohna/Library/Application Support/Aquamacs Emacs/scratch buffer") (tab "data.template.yml" "/Users/davjohna/Desktop/data.template.yml"))))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (23271 13390 534667 0) (desktop . "206") "davjohna@f45c89a7c34f.ant.amazon.com" nil nil ((((font-backend mac-ct ns) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 3) (right-fringe . 11) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (horizontal-scroll-bars . t) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility . t) (environment) (mouse-color . "black") (fringe) (frameset--id . "BDDD-C9A1-3853-C82B") (frameset--mini t . t) (height . 83) (width . 172) (modeline . t) (minibuffer . t) (unsplittable) (left . 1280) (top . 23) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "f45c89a7c34f.ant.amazon.com") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 4) (min-width-ignore . 7) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 51) (min-pixel-width-ignore . 43) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 1233) (pixel-height . 1312) (total-width . 176) (total-height . 82) (normal-height . 1.0) (normal-width . 1.0) (buffer "data.template.yml" (selected . t) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/Desktop/data.template.yml" "~/Downloads/0090965329_1473788216_s3_cache_tables_reader.py"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 206
+  "/Users/davjohna/Desktop/data.template.yml"
+  "data.template.yml"
+  'fundamental-mode
+  nil
+  1
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . undecided-unix)))
+
+(desktop-create-buffer 206
+  "/Users/davjohna/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(visual-line-mode smart-spacing-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8) (truncate-lines)))
+
